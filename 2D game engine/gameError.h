51ae8755e1d9b9083a8d367593f6d5a5bb038d73
ihaddy <ihaddy@gmail.com>
2015-01-23 14:00:47 -0500

    finished up the Direct-X window, numerous changes. It's currently in a working state, but the code could be analysed better

--- 2D game engine/gameError.h
+++ 2D game engine/gameError.h
@@ -1,5 +1,5 @@
-#ifndef _GAMEERROR_H //if not defined, then define. this prevents multiple definitions
-#define _GAMEERROR_H 
+#ifndef _GAMEERROR_H            // prevent multiple definitions if this 
+#define _GAMEERROR_H            // ..file is included in more than one place
 #define WIN32_LEAN_AND_MEAN
 
 #include <string>
@@ -7,53 +7,42 @@
 
 namespace gameErrorNS
 {
-	//These are the error codes
-	//negative numbers imply a fatal error, game MAY shut down
-	//positive numbers are warnings, game does NOT shut down
-
+	// Error codes
+	// Negative numbers are fatal errors that may require the game to be shutdown.
+	// Positive numbers are warnings that do not require the game to be shutdown.
 	const int FATAL_ERROR = -1;
 	const int WARNING = 1;
-
 }
 
-//This is the game error class. It will be thrown when the engine detects an error
-// It inherits from the std exception library
-//  The ":" is the Inheritance operator, the "::" is the scope operator
-
+// Game Error class. Thrown when an error is detected by the game engine.
+// Inherits from std::exception
 class GameError : public std::exception
-
 {
 private:
-
-	int errorCode;
+	int     errorCode;
 	std::string message;
-
 public:
-	//default constructor
+	// default constructor
 	GameError() throw() :errorCode(gameErrorNS::FATAL_ERROR), message("Undefined Error in game.") {}
-	//the copy constructor
+	// copy constructor
 	GameError(const GameError& e) throw() : std::exception(e), errorCode(e.errorCode), message(e.message) {}
-	//the constructor with args
+	// constructor with args
 	GameError(int code, const std::string &s) throw() :errorCode(code), message(s) {}
-	//assignment operator
+	// assignment operator
 	GameError& operator= (const GameError& rhs) throw()
 	{
 		std::exception::operator=(rhs);
 		this->errorCode = rhs.errorCode;
-		this->errorCode = rhs.message;
-
+		this->message = rhs.message;
 	}
-
-	//Destructor
+	// destructor
 	virtual ~GameError() throw() {};
 
-	//override what from the base class
-
+	// override what from base class
 	virtual const char* what() const throw() { return this->getMessage(); }
 
 	const char* getMessage() const throw() { return message.c_str(); }
 	int getErrorCode() const throw() { return errorCode; }
-
 };
 
 #endif
--- 2D game engine/graphics.h
+++ 2D game engine/graphics.h
@@ -1,89 +1,62 @@
-#ifndef _GRAPHICS_H //literally, if not defined, then define
-#define _GRAPHICS_H
+#ifndef _GRAPHICS_H             // prevent multiple definitions if this 
+#define _GRAPHICS_H             // ..file is included in more than one place
 #define WIN32_LEAN_AND_MEAN
 
-#ifdef _DEBUG //literally, IF DEFINED, then define
+#ifdef _DEBUG
 #define D3D_DEBUG_INFO
 #endif
-
-#include <d3d.h>
-//programmer defined headers, as indicated by the " instead of the <> characters around them
+#include <d3d9.h>
 #include "constants.h"
 #include "gameError.h"
 
-
-//These are DirectX pointer types
-
+// DirectX pointer types
 #define LP_3DDEVICE LPDIRECT3DDEVICE9
-#define LP_3D		LPDIRECT3D9
+#define LP_3D       LPDIRECT3D9
 
-// Color Dfines
-
+// Color defines
 #define COLOR_ARGB DWORD
-
-//This needs to be researched more, the syntax is atrocious, there could be a better way to implement
 #define SETCOLOR_ARGB(a,r,g,b) \
-	((COLOR_ARGB)((((a)&0xff)<<24)|(((r)&0xff)<<16)|(((g)&0xff)<<8)|((b)&0xff)))
+    ((COLOR_ARGB)((((a)&0xff)<<24)|(((r)&0xff)<<16)|(((g)&0xff)<<8)|((b)&0xff)))
 
-
-//class prototype, built upon in graphics.cpp
-
-class Graphics{
-
-	//these are private, and only the graphics class itself can access them
-	//ALTHOUGH ALL PRIVATE VARIABLES ARE TRANSFERRED TO ANY INHERITANCES, THEY ARE JUST NO EDITABLE IN THE INHERITANCE
-	// on a final note, the private members ARE accessible from WITHIN the public members of the graphics class
-
-private: 
-
-// The pointers for directX
-	LP_3D	direct2d;
+class Graphics
+{
+private:
+	// DirectX pointers and stuff
+	LP_3D       direct3d;
 	LP_3DDEVICE device3d;
 	D3DPRESENT_PARAMETERS d3dpp;
 
-	//other varriables needed
+	// other variables
+	HRESULT     result;         // standard Windows return codes
+	HWND        hwnd;
+	bool        fullscreen;
+	int         width;
+	int         height;
 
-	HRESULT	result; //This is the standard window handler return code
-	HWND	hwnd; //Standard windows handler
-	bool	fullscreen; //undefined initially
-	int		width; //undefined initially, and then filled in from the constructor in graphics.cpp
-	int		height; // undefined intially, and then filled in from the constructor in graphics.cpp
+	// (For internal engine use only. No user serviceable parts inside.)
+	// Initialize D3D presentation parameters
+	void    initD3Dpp();
 
-	//this part is supposedly for internal engine use only. "no user serviceable parts inside.)
-	//Initialization of the D3D presentation parameters
-
-	void initD3Dpp();
-
-	//These parts of the class are, meaning they are accessible to anything
 public:
-
-	//The constructor 
-
+	// Constructor
 	Graphics();
 
-	//The Destructor
-
+	// Destructor
 	virtual ~Graphics();
 
-	//How to release the direct3d and device3d
+	// Releases direct3d and device3d.
+	void    releaseAll();
 
-	void releaseAll();
+	// Initialize DirectX graphics
+	// Throws GameError on error
+	// Pre: hw = handle to window
+	//      width = width in pixels
+	//      height = height in pixels
+	//      fullscreen = true for full screen, false for window
+	void    initialize(HWND hw, int width, int height, bool fullscreen);
 
-	//How to initialize the DirectX graphics
-	//If there's an error, it'll throw GameError
-	// hw = handle to the window
-	// width is in PIXELS
-	// height in PIXELS
-	//fullscreen = true for yes, false for windowed
-	//note the void means that the function does NOT return a value.
-	void initialize(HWND hw, int width, int height, bool fullscreen);
-
-	//Display the offscreen backbuffer to the screen, similar to "flipping the screen"
-
+	// Display the offscreen backbuffer to the screen.
 	HRESULT showBackbuffer();
-
-
-	
 };
 
 #endif
